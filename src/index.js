3d16560a1de5657e804f7699be4bf0535a83a869
Jessica Nelson <yahwehschica@yahoo.com>
2021-10-21 13:18:07 -0400

    Assignment Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+const form = document.querySelector("form")
+const ulList= document.getElementById("tasks")
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.addEventListener("submit", createList)
 });
+
+function createList(todo) {
+  todo.preventDefault()
+  const task = document.querySelector("#new-task-description")
+  const newList = document.createElement("li")
+  newList.textContent = task.value
+  ulList.append(newList)  
+  task.value = " "
+}
+
+
+
+
+
+
+// <form id="create-task-form" action="#" method="POST">
+//       <label for="new-task-description">Task description:</label>
+//       <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
+//       <input type="submit" value="Create New Task">
+//     </form>
